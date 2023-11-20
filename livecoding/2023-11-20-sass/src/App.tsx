936e0b00a7055ea08058bf7f24d5b9dc04ae8cd3
Timo Bickmann (MB Air) <bickmann.timo@gmail.com>
2023-11-20 11:23:45 +0100

    commit

--- livecoding/2023-11-20-sass/src/App.tsx
+++ livecoding/2023-11-20-sass/src/App.tsx
@@ -3,6 +3,10 @@ function App() {
   return (
     <>
       <h1>SASS with @import</h1>
+      <p className="error-text spacing">This is an error.</p>
+      <p className="info-text spacing">This is a info.</p>
+      <p className="normal-text spacing">This is a normal text.</p>
+      <p className="faded-text spacing">This is an faded text.</p>
     </>
   )
 }
